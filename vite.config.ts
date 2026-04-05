1b95e7cc3b716e76005acd7260bc826742e1311b
AR Along <adolf.rey.along@gmail.com>
2026-04-06 02:09:02 +0800

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
@@ -10,5 +12,39 @@ export default defineConfig({
     ],
     port: 7239,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'AR Along — Instapay QR Portal',
+        short_name: 'Transfer QR',
+        description: 'Scan and Send Money instantly to my accounts via Instapay.',
+        theme_color: '#ffffff',
+        background_color: '#050505',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
+
